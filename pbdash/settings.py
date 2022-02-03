07d10be258f17899bedae8faa8fe7441d613547f
jeaguil <18241ja@gmail.com>
2022-02-03 14:33:31 -0800

    Home View Set up

--- pbdash/settings.py
+++ pbdash/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Created apps ->
+    "pages",
 ]
 
 MIDDLEWARE = [
@@ -57,7 +59,7 @@ ROOT_URLCONF = "pbdash.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
