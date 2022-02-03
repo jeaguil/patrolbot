d5e59eb84e20129ebe73a626a048f0218cc6ec40
jeaguil <18241ja@gmail.com>
2022-02-02 18:50:50 -0800

    Black formatter and Local Server allowed host

--- pbdash/wsgi.py
+++ pbdash/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pbdash.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pbdash.settings")
 
 application = get_wsgi_application()
