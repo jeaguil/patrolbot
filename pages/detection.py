5619a8fd45b7d1470703665bf4b770ec22185d74
jeaguil <18241ja@gmail.com>
2022-03-07 15:22:50 -0800

    Format page

--- pages/detection.py
+++ pages/detection.py
@@ -33,13 +33,13 @@ model_weights = os.path.join(
 
 
 class KinesisStream(object):
-	def __init__(self, url, yolo, colors):
-		self.url = url
-		self.yolo = yolo
-		self.colors = colors
+    def __init__(self, url, yolo, colors):
+        self.url = url
+        self.yolo = yolo
+        self.colors = colors
 
-	def get_frame(self):
-		return runYolo(self.url, self.yolo, self.colors)
+    def get_frame(self):
+        return runYolo(self.url, self.yolo, self.colors)
 
 
 # function code adapted
@@ -76,205 +76,205 @@ def hls_stream():
 
     return url
 
+
 # create yolo model and return it
 def get_yolo():
-	# Torch code adapted from https://github.com/akash-agni/Real-Time-Object-Detection/blob/main/Object_Detection_Youtube.py
-	model_weight_path = model_weights
-	model = torch.hub.load("ultralytics/yolov5", "custom", model_weight_path)
-	return model
-	
+    # Torch code adapted from https://github.com/akash-agni/Real-Time-Object-Detection/blob/main/Object_Detection_Youtube.py
+    model_weight_path = model_weights
+    model = torch.hub.load("ultralytics/yolov5", "custom", model_weight_path)
+    return model
+
 
 # create action model and return it
 def get_action_model():
-	# code adapted from https://cv.gluon.ai/build/examples_action_recognition/demo_slowfast_kinetics400.html
-	# Load trained Slow Fast Model
-	model_name = "slowfast_4x16_resnet50_kinetics400"
-	net = get_model(model_name, nclass=400, pretrained=True)
-	print("%s model is successfully loaded." % model_name)
-	return net
+    # code adapted from https://cv.gluon.ai/build/examples_action_recognition/demo_slowfast_kinetics400.html
+    # Load trained Slow Fast Model
+    model_name = "slowfast_4x16_resnet50_kinetics400"
+    net = get_model(model_name, nclass=400, pretrained=True)
+    print("%s model is successfully loaded." % model_name)
+    return net
+
 
 # run action detection based on video stream from kinesis
 def run_action_detection(url, net):
-	# get list of frame numbers for fast portion of neural network
-	fast_frame_id_list = range(0, 64, 2)
-	# get list of frame numbers for slow portion of neural network
-	slow_frame_id_list = range(0, 64, 16)
-	# combine the two lists
-	frame_id_list = list(fast_frame_id_list) + list(slow_frame_id_list)
+    # get list of frame numbers for fast portion of neural network
+    fast_frame_id_list = range(0, 64, 2)
+    # get list of frame numbers for slow portion of neural network
+    slow_frame_id_list = range(0, 64, 16)
+    # combine the two lists
+    frame_id_list = list(fast_frame_id_list) + list(slow_frame_id_list)
 
-	# frames captured initially set to 0
-	frameCounter = 0
-	# declare a list of frames
-	clip_input = []
-	# declare a list of slow frames
-	slow_input = []
-	while True:
-		vcap = cv2.VideoCapture(url)
-		# Capture frame-by-frame
-		ret, frame = vcap.read()
+    # frames captured initially set to 0
+    frameCounter = 0
+    # declare a list of frames
+    clip_input = []
+    # declare a list of slow frames
+    slow_input = []
+    while True:
+        vcap = cv2.VideoCapture(url)
+        # Capture frame-by-frame
+        ret, frame = vcap.read()
 
-		if ret:
+        if ret:
 
-			# Flip video frame, so it isn't reversed
-			image = cv2.flip(frame, 1)
+            # Flip video frame, so it isn't reversed
+            image = cv2.flip(frame, 1)
 
-			# if the current frame was selected by the frame_id_list
-			# add the frame to the list of frames
-			if frameCounter in frame_id_list:
-				# frame is originally 720 x 1280 x 3
-				# form is height x width x dimensions
-				# resize frame to required 224 x 224
-				frame = cv2.resize(frame, (224, 224))
-				# convert frame to tensor
-				imgTensor = T.ToTensor()(frame)
-				# create a transformation
-				# normalize by mean and standard deviation
-				transform = T.Normalize(
-				    mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
-				)
-				# apply transformation to tensor
-				normalizedTensor = transform(imgTensor)
-				# change tensor back to numpy array
-				frame = normalizedTensor.cpu().detach().numpy()
-				# frame has format 224 x 224 x 3
-				# append the frame to the list of frames
-				clip_input.append(frame)
+            # if the current frame was selected by the frame_id_list
+            # add the frame to the list of frames
+            if frameCounter in frame_id_list:
+                # frame is originally 720 x 1280 x 3
+                # form is height x width x dimensions
+                # resize frame to required 224 x 224
+                frame = cv2.resize(frame, (224, 224))
+                # convert frame to tensor
+                imgTensor = T.ToTensor()(frame)
+                # create a transformation
+                # normalize by mean and standard deviation
+                transform = T.Normalize(
+                    mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
+                )
+                # apply transformation to tensor
+                normalizedTensor = transform(imgTensor)
+                # change tensor back to numpy array
+                frame = normalizedTensor.cpu().detach().numpy()
+                # frame has format 224 x 224 x 3
+                # append the frame to the list of frames
+                clip_input.append(frame)
 
-				# if the frame is in the list of slow frame numbers
-				# append to the slow input list
-				if frameCounter in slow_frame_id_list:
-				    slow_input.append(frame)
+                # if the frame is in the list of slow frame numbers
+                # append to the slow input list
+                if frameCounter in slow_frame_id_list:
+                    slow_input.append(frame)
 
-			# increment frame counter to keep track
-			frameCounter += 1
+            # increment frame counter to keep track
+            frameCounter += 1
 
-			# if there are enough frames to run the action detection
-			# apply it
-			if frameCounter >= 70:
-				# combine the video frames into one list
-				# goes from 32 x 3 x 224 x 224 to 36 x 3 x 224 x 224
-				clip_input = np.vstack((clip_input, slow_input))
-				# join arrays on first axis
-				clip_input = np.stack(clip_input, axis=0)
-				# add a new dimensions and reshape
-				clip_input = clip_input.reshape((-1,) + (36, 3, 224, 224))
-				# finally array into 1 x 3 x 36 x 224 x 224
-				# form is channels x frames x height x width
-				clip_input = np.transpose(clip_input, (0, 2, 1, 3, 4))
+            # if there are enough frames to run the action detection
+            # apply it
+            if frameCounter >= 70:
+                # combine the video frames into one list
+                # goes from 32 x 3 x 224 x 224 to 36 x 3 x 224 x 224
+                clip_input = np.vstack((clip_input, slow_input))
+                # join arrays on first axis
+                clip_input = np.stack(clip_input, axis=0)
+                # add a new dimensions and reshape
+                clip_input = clip_input.reshape((-1,) + (36, 3, 224, 224))
+                # finally array into 1 x 3 x 36 x 224 x 224
+                # form is channels x frames x height x width
+                clip_input = np.transpose(clip_input, (0, 2, 1, 3, 4))
 
-				# make the prediction based on the frames
-				pred = net(nd.array(clip_input))
+                # make the prediction based on the frames
+                pred = net(nd.array(clip_input))
 
-				actionClasses = net.classes
-				topK = 5
-				ind = nd.topk(pred, k=topK)[0].astype("int")
-				predictions = []
+                actionClasses = net.classes
+                topK = 5
+                ind = nd.topk(pred, k=topK)[0].astype("int")
+                predictions = []
 
-				# collect the top 5 predictions
-				for i in range(topK):
-				    predictions.append(
-					[
-					    actionClasses[ind[i].asscalar()],
-					    nd.softmax(pred)[0][ind[i]].asscalar(),
-					]
-				    )
+                # collect the top 5 predictions
+                for i in range(topK):
+                    predictions.append(
+                        [
+                            actionClasses[ind[i].asscalar()],
+                            nd.softmax(pred)[0][ind[i]].asscalar(),
+                        ]
+                    )
 
-				# extract the action with the highest confidence level
-				bestPrediction = predictions[0]
-				bestAction = bestPrediction[0]
-				bestConfidence = bestPrediction[1]
+                # extract the action with the highest confidence level
+                bestPrediction = predictions[0]
+                bestAction = bestPrediction[0]
+                bestConfidence = bestPrediction[1]
 
-				# print out best action for stats
-				print(bestAction, " with confidence ", bestConfidence)
+                # print out best action for stats
+                print(bestAction, " with confidence ", bestConfidence)
 
-				# reset frame counter to 0
-				frameCounter = 0
-				# empty the frames lists so they can be collected again
-				# declare a list of frames
-				clip_input = []
-				# declare a list of slow frames
-				slow_input = []
+                # reset frame counter to 0
+                frameCounter = 0
+                # empty the frames lists so they can be collected again
+                # declare a list of frames
+                clip_input = []
+                # declare a list of slow frames
+                slow_input = []
+
 
 # run yolo based on video stream from kinesis
 def runYolo(url, model, colors):
-	vcap = cv2.VideoCapture(url)
+    vcap = cv2.VideoCapture(url)
 
-	# Extract the names of the classes for trained the YoloV5 model
-	classes = model.names
-	class_ids = [0, 1, 2, 3]
-	COLORS = colors
+    # Extract the names of the classes for trained the YoloV5 model
+    classes = model.names
+    class_ids = [0, 1, 2, 3]
+    COLORS = colors
 
-	# Capture frame-by-frame
-	ret, frame = vcap.read()
+    # Capture frame-by-frame
+    ret, frame = vcap.read()
 
-	if ret:
+    if ret:
 
-		# Flip video frame, so it isn't reversed
-		image = cv2.flip(frame, 1)
+        # Flip video frame, so it isn't reversed
+        image = cv2.flip(frame, 1)
 
-		# If model is turned on and the object is initialized
-		# run object detection on each frame
+        # If model is turned on and the object is initialized
+        # run object detection on each frame
 
-		################################################################
-		# TORCH OBJECT DETECTION
-		################################################################
+        ################################################################
+        # TORCH OBJECT DETECTION
+        ################################################################
 
-		# Get dimensions of the current video frame
-		x_shape = image.shape[1]
-		y_shape = image.shape[0]
+        # Get dimensions of the current video frame
+        x_shape = image.shape[1]
+        y_shape = image.shape[0]
 
-		# Apply the Torch YoloV5 model to this frame
-		results = model(image)
+        # Apply the Torch YoloV5 model to this frame
+        results = model(image)
 
-		# Extract the labels and coordinates of the bounding boxes
-		labels, cords = (
-		results.xyxyn[0][:, -1].numpy(),
-		results.xyxyn[0][:, :-1].numpy(),
-		)
+        # Extract the labels and coordinates of the bounding boxes
+        labels, cords = (
+            results.xyxyn[0][:, -1].numpy(),
+            results.xyxyn[0][:, :-1].numpy(),
+        )
 
-		numberOfLabels = len(labels)
+        numberOfLabels = len(labels)
 
-		# declare empty array of objects found
-		objectsFound = []
+        # declare empty array of objects found
+        objectsFound = []
 
-		for i in range(numberOfLabels):
-			row = cords[i]
-			# Get the class number of current label
-			class_number = int(labels[i])
-			# Index colors list with current label number
-			color = COLORS[class_ids[class_number]]
+        for i in range(numberOfLabels):
+            row = cords[i]
+            # Get the class number of current label
+            class_number = int(labels[i])
+            # Index colors list with current label number
+            color = COLORS[class_ids[class_number]]
 
-			# If confidence level is greater than 0.2
-			if row[4] >= 0.4:
-			    # Get label to send to dashbaord
-			    label = classes[class_number]
-			    x1, y1, x2, y2 = (
-				int(row[0] * x_shape),
-				int(row[1] * y_shape),
-				int(row[2] * x_shape),
-				int(row[3] * y_shape),
-			    )
+            # If confidence level is greater than 0.2
+            if row[4] >= 0.4:
+                # Get label to send to dashbaord
+                label = classes[class_number]
+                x1, y1, x2, y2 = (
+                    int(row[0] * x_shape),
+                    int(row[1] * y_shape),
+                    int(row[2] * x_shape),
+                    int(row[3] * y_shape),
+                )
 
-			    # append coords and label so it can be analyzed
-			    objectsFound.append([x1, y1, x2, y2, label])
+                # append coords and label so it can be analyzed
+                objectsFound.append([x1, y1, x2, y2, label])
 
+                # If global enable flag is set true then show boxes
+                # Draw bounding box
+                cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
+                # Give bounding box a text label
+                cv2.putText(
+                    image,
+                    str(classes[int(labels[i])]),
+                    (int(x1) - 10, int(y1) - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    2,
+                    color,
+                    2,
+                )
 
-			    # If global enable flag is set true then show boxes
-			    # Draw bounding box
-			    cv2.rectangle(
-				image, (int(x1), int(y1)), (int(x2), int(y2)), color, 2
-			    )
-			    # Give bounding box a text label
-			    cv2.putText(
-				image,
-				str(classes[int(labels[i])]),
-				(int(x1) - 10, int(y1) - 10),
-				cv2.FONT_HERSHEY_SIMPLEX,
-				2,
-				color,
-				2,
-			    )
-
-		# return the resulting image
-		_, jpeg = cv2.imencode(".jpg", image)
-		return jpeg.tobytes()
+        # return the resulting image
+        _, jpeg = cv2.imencode(".jpg", image)
+        return jpeg.tobytes()
