9633ec13b82faaf141e4d34771c04d323b4013a4
Max-Orloff <93732962+Max-Orloff@users.noreply.github.com>
2022-03-09 19:29:59 -0800

    Added Training code

--- ActionDetectionTraining/ActionDetectionTraining.py
+++ ActionDetectionTraining/ActionDetectionTraining.py
@@ -1,1 +1,120 @@
+from __future__ import division
 
+import argparse, time, logging, os, sys, math
+
+import numpy as np
+import mxnet as mx
+import gluoncv as gcv
+from mxnet import gluon, nd, init, context
+from mxnet import autograd as ag
+from mxnet.gluon import nn
+from mxnet.gluon.data.vision import transforms
+
+from gluoncv.data.transforms import video
+from gluoncv.data import VideoClsCustom
+from gluoncv.model_zoo import get_model
+from gluoncv.utils import makedirs, LRSequential, LRScheduler, split_and_load, TrainingHistory
+
+# Replace the values of ``root`` and ``setting`` within VideoClsCustom to data directory and prepared text file.
+
+num_gpus = 1
+ctx = [mx.gpu(i) for i in range(num_gpus)]
+transform_train = video.VideoGroupTrainTransform(size=(224, 224), scale_ratios=[1.0, 0.8], mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+per_device_batch_size = 5
+num_workers = 0
+batch_size = per_device_batch_size * num_gpus
+
+train_dataset = VideoClsCustom(root=('/'),
+                               setting=('/train.txt'),
+                               train=True,
+                               new_length=32,
+                               video_loader=True,
+                               use_decord=True,
+                               transform=transform_train)
+print('Load %d training samples.' % len(train_dataset))
+train_data = gluon.data.DataLoader(train_dataset, batch_size=batch_size,
+                                   shuffle=True, num_workers=num_workers)
+
+# Change nclass in get_model to the number of classes within dataset
+
+net = get_model(name='i3d_resnet50_v1_custom', nclass=2)
+net.collect_params().reset_ctx(ctx)
+print(net)
+
+# Learning rate decay factor
+lr_decay = 0.1
+# Epochs where learning rate decays
+lr_decay_epoch = [40, 80, 100]
+
+# Stochastic gradient descent
+optimizer = 'sgd'
+# Set parameters
+optimizer_params = {'learning_rate': 0.001, 'wd': 0.0001, 'momentum': 0.9}
+
+# Define our trainer for net
+trainer = gluon.Trainer(net.collect_params(), optimizer, optimizer_params)
+
+# For classification tasks, use softmax cross entropy as the
+# loss function.
+
+loss_fn = gluon.loss.SoftmaxCrossEntropyLoss()
+
+# Accuracy to measure progression during training
+
+train_metric = mx.metric.Accuracy()
+train_history = TrainingHistory(['training-acc'])
+
+# Change number of epochs to desired amount
+epochs = 25
+lr_decay_count = 0
+
+for epoch in range(epochs):
+    tic = time.time()
+    train_metric.reset()
+    train_loss = 0
+
+    # Learning rate decay
+    if epoch == lr_decay_epoch[lr_decay_count]:
+        trainer.set_learning_rate(trainer.learning_rate*lr_decay)
+        lr_decay_count += 1
+
+    # Loop through each batch of training data
+    for i, batch in enumerate(train_data):
+        # Extract data and label
+        data = split_and_load(batch[0], ctx_list=ctx, batch_axis=0)
+        label = split_and_load(batch[1], ctx_list=ctx, batch_axis=0)
+
+        # AutoGrad
+        with ag.record():
+            output = []
+            for _, X in enumerate(data):
+                X = X.reshape((-1,) + X.shape[2:])
+                pred = net(X)
+                output.append(pred)
+            loss = [loss_fn(yhat, y) for yhat, y in zip(output, label)]
+
+        # Backpropagation
+        for l in loss:
+            l.backward()
+
+        # Optimize
+        trainer.step(batch_size)
+
+        # Update metrics
+        train_loss += sum([l.mean().asscalar() for l in loss])
+        train_metric.update(label, output)
+
+        if i == 100:
+            break
+
+    name, acc = train_metric.get()
+
+    # Update history and print metrics
+    train_history.update([acc])
+    print('[Epoch %d] train=%f loss=%f time: %f' %
+        (epoch, acc, train_loss / (i+1), time.time()-tic))
+
+# We can plot the metric scores with:
+train_history.plot()
+# Save parameters for model to use elsewhere
+net.save_parameters('ActionDetection.params')
