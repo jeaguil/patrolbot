9b74a0fea5015c209e5cbb51ee6a48af7f996dcd
jeaguil <18241ja@gmail.com>
2022-03-23 20:14:59 -0700

    Model migrations for checkbox db integrations

--- pages/models.py
+++ pages/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+class DashboardVideoSettings(models.Model):
+    setting = models.CharField(max_length=100)
+    switch = models.BooleanField('switch', default=True)
+    
+    def __str__(self):
+        return self.setting
+    
+class DashboardModelSettings(models.Model):
+    setting = models.CharField(max_length=100)
+    switch = models.BooleanField('switch', default=True)
+    
+    def __str__(self):
+        return self.setting
