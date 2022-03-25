f05e66f824d52cae35604d10936c75cc85033e6b
Connor-callister <ctcallister@gmail.com>
2022-03-25 12:15:07 -0700

    added gps parse code for Pi

---
+++ picode/GPS_Test.py
@@ -0,0 +1,38 @@
+#Module to parse GPS Dongle input into latitude and longitude coordinates
+
+from gps import *
+import time
+
+
+#reads in gps data and extracts lat/long info
+def getLocation(gpsd):   
+    data = gpsd.next()
+    if data['class'] == 'TPV':
+        lon = getattr(data, 'lon', "Unknown")
+        lat = getattr(data, 'lat', "Unknown")
+        return lon, lat
+
+
+active = True
+gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+
+
+#testing gps parse code
+try:
+    while active:       
+        coords = getLocation(gpsd)
+        
+        #Handles faulty readings
+        if (coords == None):
+            pass
+        else:
+            longitude = coords[0]
+            latitude = coords[1]
+            print("Longitude: ", longitude)
+            print("Latitude: ", latitude)
+
+        #adjust sleep time for frequency of readings
+        time.sleep(2)
+except (KeyboardInterrupt):
+    active = False
+    print("Exiting")
