de58ecfae11ddb310a683f500e27aeb04d0ec382
jeaguil <18241ja@gmail.com>
2022-03-24 18:13:34 -0700

    Update db migrations

--- pages/models.py
+++ pages/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 class DashboardVideoSettings(models.Model):
-    setting = models.CharField(max_length=100)
+    name_id = models.CharField(max_length=100, blank=False, default='', unique=True)
+    setting = models.CharField(max_length=100, blank=False, default='', unique=True)
     switch = models.BooleanField('switch', default=True)
     
     def __str__(self):
-        return self.setting
+        return self.name_id
     
 class DashboardModelSettings(models.Model):
-    setting = models.CharField(max_length=100)
+    name_id = models.CharField(max_length=100, blank=False, default='', unique=True)
+    setting = models.CharField(max_length=100, blank=False, default='', unique=True)
     switch = models.BooleanField('switch', default=True)
     
     def __str__(self):
-        return self.setting
+        return self.name_id
