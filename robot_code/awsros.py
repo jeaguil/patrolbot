0bf51c80b6802b67df460c54bea94b9cedeb6491
Michael Stepzinski <39574141+MichaelStepzinski@users.noreply.github.com>
2022-03-30 23:00:26 -0700

    merged and edited gps code into awsros code, making awsrosgps

--- robot_code/awsros.py
+++ robot_code/awsros.py
@@ -5,6 +5,7 @@ import sys
 import json
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 from movement import MovementManager
+from gps import *
 
 class PatrolBotMovement:
 	def __init__(self):
@@ -27,20 +28,20 @@ class PatrolBotMovement:
 		self.movement_manager.set_max_angular_velocity(0.5)
 		self.movement_manager.turn(1.35)
 
-class MsgSys:
-    #robot = PatrolBotMovement()
+#class MsgSys:
+#    #robot = PatrolBotMovement()
     
-    def __init__(self):
-        self.myMQTTClient = AWSIoTMQTTClient("clientid")
-        self.myMQTTClient.configureEndpoint("aa03kvhkub5ls-ats.iot.us-west-2.amazonaws.com", 8883)
-        self.myMQTTClient.configureCredentials("/home/ubuntu/AWSIoT/AmazonRootCA1.pem", "/home/ubuntu/AWSIoT/private.pem.key", "/home/ubuntu/AWSIoT/certificate.pem.crt") #Set path for Root CA and provisioning claim credentials
-        self.myMQTTClient.configureOfflinePublishQueueing(-1)
-        self.myMQTTClient.configureDrainingFrequency(2)
-        self.myMQTTClient.configureConnectDisconnectTimeout(10)
-        self.myMQTTClient.configureMQTTOperationTimeout(5)
-        print ('Initiating IoT Core Topic ...')
-        self.myMQTTClient.connect()
-        self.myMQTTClient.subscribe("robot/control", 1, handle_control)
+#    def __init__(self):
+#        self.myMQTTClient = AWSIoTMQTTClient("clientid")
+#        self.myMQTTClient.configureEndpoint("aa03kvhkub5ls-ats.iot.us-west-2.amazonaws.com", 8883)
+#        self.myMQTTClient.configureCredentials("/home/ubuntu/AWSIoT/AmazonRootCA1.pem", "/home/ubuntu/AWSIoT/private.pem.key", "/home/ubuntu/AWSIoT/certificate.pem.crt") #Set path for Root CA and provisioning claim credentials
+#        self.myMQTTClient.configureOfflinePublishQueueing(-1)
+#        self.myMQTTClient.configureDrainingFrequency(2)
+#        self.myMQTTClient.configureConnectDisconnectTimeout(10)
+#        self.myMQTTClient.configureMQTTOperationTimeout(5)
+#        print ('Initiating IoT Core Topic ...')
+#        self.myMQTTClient.connect()
+#        self.myMQTTClient.subscribe("robot/control", 1, handle_control)
         
 def handle_control(self, params, packet):
     payload = json.loads(packet.payload)
@@ -63,7 +64,59 @@ def handle_control(self, params, packet):
 
 robot = PatrolBotMovement()
 
+def getLocation(gpsd):   
+    data = gpsd.next()
+    if data['class'] == 'TPV':
+        lon = getattr(data, 'lon', "Unknown")
+        lat = getattr(data, 'lat', "Unknown")
+        return lon, lat
+
+active = True
+gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+
 if __name__ == '__main__':
-    msg = MsgSys()
-    while(True):
-        time.sleep(1) 
+    #msg = MsgSys()
+    myMQTTClient = AWSIoTMQTTClient("clientid")
+    myMQTTClient.configureEndpoint("aa03kvhkub5ls-ats.iot.us-west-2.amazonaws.com", 8883)
+    myMQTTClient.configureCredentials("/home/ubuntu/patrolbot/certificates/AmazonRootCA1.pem", "/home/ubuntu/patrolbot/certificates/private.pem.key", "/home/ubuntu/patrolbot/certificates/certificate.pem.crt") #Set path for Root CA and provisioning claim credentials
+    myMQTTClient.configureOfflinePublishQueueing(-1)
+    myMQTTClient.configureDrainingFrequency(2)
+    myMQTTClient.configureConnectDisconnectTimeout(10)
+    myMQTTClient.configureMQTTOperationTimeout(5)
+    print ('Initiating IoT Core Topic ...')
+    myMQTTClient.connect()
+    myMQTTClient.subscribe("robot/control", 1, handle_control)
+    
+    active = True
+    # handle gps publishing
+    try:
+        while(active):
+        #time.sleep(1)    
+            coords = getLocation(gpsd)
+        
+        #Handles faulty readings
+            if (coords == None):
+                print("No coords available!")
+                pass
+            else:
+                longitude = coords[0]
+                latitude = coords[1]
+                print("latlong:" + str(longitude) + "," + str(latitude))
+                TOPIC = "robot/location"
+                MESSAGE = str(longitude) + "," + str(latitude)
+                data = "{} [{}]".format(MESSAGE, 0)
+                message = {"message" : data}
+                myMQTTClient.publish(TOPIC, json.dumps(message), 1)
+                print("Printed '" + json.dumps(message) + "' to the topic: " + TOPIC)
+                #adjust sleep time for frequency of readings
+                time.sleep(10)
+            #myMQTTClient.publish(
+            #    topic="robot/location",
+            #    QoS=1,
+            #    payload='{"direction":"right"}'
+            #)
+
+        
+    except (KeyboardInterrupt):
+        active = False
+        print("Exiting") 
