1e2504cef648cc7f8829cce835b5d078533854f3
banuelos-brandon <66798236+banuelos-brandon@users.noreply.github.com>
2022-04-01 18:02:56 -0700

    Changed the location of yolo

--- pages/views.py
+++ pages/views.py
@@ -19,6 +19,19 @@ from . import detection
 
 from picode import pi_publisher
 
+import os
+import cv2
+from . import loggers
+from datetime import datetime
+from django.conf import settings
+pacific_tz = pytz.timezone('US/Pacific')
+model_weights = os.path.join(
+    settings.BASE_DIR, "model_weights/patrolNanoWeights.pt")
+model_weight_path = model_weights
+yolo = torch.hub.load("ultralytics/yolov5", "custom", model_weight_path)
+classes = yolo.names
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+
 
 def welcome_view(request):
     return render(request, "pages/welcome.html", {})
@@ -115,7 +128,8 @@ def dashboard_view(request):
             obj.save()
 
         try:
-            person_detect = DashboardModelSettings.objects.get(name_id="Person")
+            person_detect = DashboardModelSettings.objects.get(
+                name_id="Person")
             bike_detect = DashboardModelSettings.objects.get(name_id="Bike")
             angle_grinder_detect = DashboardModelSettings.objects.get(
                 name_id="Angle Grinders"
@@ -163,20 +177,25 @@ def dashboard_settings_view(request):
             DashboardVideoSettings.objects.all().order_by("id").update(switch=False)
 
             # Get list of checkbed box id's
-            video_settings_id_list = request.POST.getlist("dashboard_video_settings")
-            model_settings_id_list = request.POST.getlist("dashboard_model_settings")
+            video_settings_id_list = request.POST.getlist(
+                "dashboard_video_settings")
+            model_settings_id_list = request.POST.getlist(
+                "dashboard_model_settings")
 
             # Update the database
             for i in video_settings_id_list:
-                DashboardVideoSettings.objects.filter(name_id=i).update(switch=True)
+                DashboardVideoSettings.objects.filter(
+                    name_id=i).update(switch=True)
             for i in model_settings_id_list:
-                DashboardModelSettings.objects.filter(name_id=i).update(switch=True)
+                DashboardModelSettings.objects.filter(
+                    name_id=i).update(switch=True)
 
             messages.success(request, "Settings updated successfully!")
             return render(
                 request,
                 "pages/settings.html",
-                {"video_settings": video_settings, "model_settings": model_settings},
+                {"video_settings": video_settings,
+                    "model_settings": model_settings},
             )
 
         return render(
@@ -201,10 +220,138 @@ def dashboard_robot_manual_view(request):
     return render(request, "pages/robot_manual.html", {})
 
 
-def gen(cam):
+def gen(url):
+    vcap = cv2.VideoCapture(url)
     while True:
-        frame = cam.get_frame()
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
+
+        # Extract the names of the classes for trained the YoloV5 model
+        class_ids = [0, 1, 2, 3]
+
+        # Capture frame-by-frame
+        ret, frame = vcap.read()
+
+        if ret:
+            #global framesList
+            # if len(framesList) > 0:
+            #frame = framesList.pop(0)
+
+            # Flip video frame, so it isn't reversed
+            image = cv2.flip(frame, 1)
+
+            # If model is turned on and the object is initialized
+            # run object detection on each frame
+
+            ################################################################
+            # TORCH OBJECT DETECTION
+            ################################################################
+
+            # Get dimensions of the current video frame
+            x_shape = image.shape[1]
+            y_shape = image.shape[0]
+
+            # Apply the Torch YoloV5 model to this frame
+            #results = yolo(image)
+
+            # Extract the labels and coordinates of the bounding boxes
+            # labels, cords = (
+            #    results.xyxyn[0][:, -1].numpy(),
+            #    results.xyxyn[0][:, :-1].numpy(),
+            # )
+            labels = ""
+            numberOfLabels = len(labels)
+
+            # declare empty array of objects found
+            objectsFound = []
+
+            for i in range(numberOfLabels):
+                row = cords[i]
+                # Get the class number of current label
+                class_number = int(labels[i])
+                # Index colors list with current label number
+                color = COLORS[class_ids[class_number]]
+
+                # If confidence level is greater than 0.2
+                if row[4] >= 0.4:
+                    # Get label to send to dashbaord
+                    label = classes[class_number]
+                    x1, y1, x2, y2 = (
+                        int(row[0] * x_shape),
+                        int(row[1] * y_shape),
+                        int(row[2] * x_shape),
+                        int(row[3] * y_shape),
+                    )
+
+                    # if malicious item detected, send alert
+                    if label == "Angle Grinder" or label == "Bolt Cutters":
+                        # send an alert to the alerts log
+                        time_of_event = datetime.now(
+                            pacific_tz).strftime("%Y-%m-%d %H:%M:%S")
+                        seconds = int(datetime.today().timestamp() % 10)
+                        if seconds == 0:
+                            # every 10 seconds append to the log form
+
+                            loggers.objects_detected.append(
+                                [time_of_event, "Malicious item detected: " + label])
+
+                    # append coords and label so it can be analyzed
+                    objectsFound.append([x1, y1, x2, y2, label])
+
+                    # If global enable flag is set true then show boxes
+                    # Draw bounding box
+                    cv2.rectangle(image, (int(x1), int(y1)),
+                                  (int(x2), int(y2)), color, 2)
+                    # Give bounding box a text label
+                    cv2.putText(
+                        image,
+                        str(classes[int(labels[i])]),
+                        (int(x1) - 10, int(y1) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        2,
+                        color,
+                        2,
+                    )
+
+                    # send objected detected to log page
+                    time_of_event = datetime.now(
+                        pacific_tz).strftime("%Y-%m-%d %H:%M:%S")
+                    seconds = int(datetime.today().timestamp() % 10)
+                    if seconds == 0:
+                        # every 10 seconds append to the log form
+                        loggers.objects_detected.append(
+                            [time_of_event, "Object detected: " + label])
+
+            # ensure there are enough objects for action detection algorithm
+            if len(objectsFound) >= 2:
+                # iterate over items found
+                for index in range(len(objectsFound)):
+                    # check if first object is malicious
+                    if objectsFound[index][4] == 'Angle Grinder' or objectsFound[index][4] == 'Bolt Cutters':
+
+                        # check if it is bike was detected in same frame
+                        for index2 in range(len(objectsFound)):
+                            if objectsFound[index2][4] == 'Bike':
+                                x1, y1, x2, y2, label1 = objectsFound[index]
+                                x3, y3, x4, y4, label2 = objectsFound[index2]
+                                box1 = [x1, y1, x2, y2]
+                                box2 = [x3, y3, x4, y4]
+                                # if interseciton is greater than 50 percent
+                                # send a threat alert to alert logs
+                                iou = iou(box1, box2)
+                                if iou >= 0.05:
+                                    time_of_event = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                                    seconds = int(
+                                        datetime.today().timestamp() % 10)
+                                    if seconds == 0:
+                                        # every 10 seconds append to the log form
+
+                                        loggers.security_notices.append(
+                                            [time_of_event, "Potential Threat computed with confidence level of " + iou + ": " + label1 + " and " + label2 + " detected"])
+
+            # return the resulting image
+            _, jpeg = cv2.imencode(".jpg", image)
+            frame = jpeg.tobytes()
+            # return frame
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
 
 
 def phone_feed_view(request):
@@ -218,16 +365,17 @@ def kinesis_stream_view(request):
     # retrieves url on hls stream
     url = detection.hls_stream()
     # create yolo model
-    yolo = detection.get_yolo()
-    classes = yolo.names
-    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+    #yolo = detection.get_yolo()
+    #classes = yolo.names
+    #COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
     # create action detection model
     action = detection.get_action_model()
     # create thread to run action detection
-    thread = threading.Thread(target=detection.run_action_detection, args=(url, action))
+    thread = threading.Thread(
+        target=detection.run_action_detection, args=(url, action))
     thread.start()
     return StreamingHttpResponse(
-        gen(detection.KinesisStream(url, yolo, COLORS)),
+        gen(url),
         content_type="multipart/x-mixed-replace;boundary=frame",
     )
 
