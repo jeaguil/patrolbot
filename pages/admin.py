26c828318a5dc9046355d0f74ac61f9a29986671
jeaguil <18241ja@gmail.com>
2022-04-07 14:01:03 -0700

    Register database models to admin page

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import DashboardVideoSettings, DashboardModelSettings, Appearance
 
 # Register your models here.
+for i in [DashboardVideoSettings, DashboardModelSettings, Appearance]:
+    admin.site.register(i)
